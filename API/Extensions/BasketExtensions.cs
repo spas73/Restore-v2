d7894f2ad4e2251a522d6a4418bf1630d877e45c
spas73 <silviomariopastori73@gmail.com>
2025-06-13 16:47:55 +0200

    End of section 7

---
+++ API/Extensions/BasketExtensions.cs
@@ -0,0 +1,39 @@
+using System;
+using API.DTOs;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class BasketExtensions
+{
+    public static BasketDto ToDto(this Basket basket)
+    {
+        return new BasketDto
+        {
+            BasketId = basket.BasketId,
+/*             ClientSecret = basket.ClientSecret,
+            Coupon = basket.Coupon, */
+            Items = basket.Items.Select(x => new BasketItemDto
+            {
+                ProductId = x.ProductId,
+                Name = x.Product.Name,
+                Price = x.Product.Price,
+                Brand = x.Product.Brand,
+                Type = x.Product.Type,
+                PictureUrl = x.Product.PictureUrl,
+                Quantity = x.Quantity
+            }).ToList()
+        };
+    }
+
+/*     public static async Task<Basket> GetBasketWithItems(this IQueryable<Basket> query,
+        string? basketId)
+    {
+        return await query
+            .Include(x => x.Items)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.BasketId == basketId)
+                ?? throw new Exception("Cannot get basket");
+    } */
+}
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -15,9 +15,24 @@ import {
   Typography,
 } from "@mui/material";
 import { useFetchProductDetailsQuery } from "./catalogApi";
+import {
+  useAddBasketItemMutation,
+  useFetchBasketQuery,
+  useRemoveBasketItemMutation,
+} from "../basket/basketApi";
+import { useEffect, useState, type ChangeEvent } from "react";
 
 export default function ProductDetails() {
   const { id } = useParams();
+  const [removeBasketItem] = useRemoveBasketItemMutation();
+  const [addBasketItem] = useAddBasketItemMutation();
+  const { data: basket } = useFetchBasketQuery();
+  const item = basket?.items.find((x) => x.productId === +id!);
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    if (item) setQuantity(item.quantity);
+  }, [item]);
 
   const { data: product, isLoading } = useFetchProductDetailsQuery(
     id ? +id : 0
@@ -34,6 +49,20 @@ export default function ProductDetails() {
   }, [id]);*/
 
   if (!product || isLoading) return <div>Loading...</div>;
+
+  const handleUpdateBasket = () => {
+    const updateQuantity = item ? Math.abs(quantity - item.quantity) : quantity;
+    if (!item || quantity > item.quantity) {
+      addBasketItem({ product, quantity: updateQuantity });
+    } else {
+      removeBasketItem({ productId: product.id, quantity: updateQuantity });
+    }
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = +event.currentTarget.value;
+    if (value >= 0) setQuantity(value);
+  };
 
   const productDetails = [
     { label: "Name", value: product.name },
@@ -85,18 +114,23 @@ export default function ProductDetails() {
                   type="number"
                   label="Quantity in basket"
                   fullWidth
-                  defaultValue={1}
+                  value={quantity}
+                  onChange={handleInputChange}
                 />
               </Grid2>
               <Grid2 size={6}>
                 <Button
+                  onClick={handleUpdateBasket}
+                  disabled={
+                    quantity === item?.quantity || (!item && quantity === 0)
+                  }
                   sx={{ height: "55px" }}
                   color="primary"
                   size="large"
                   variant="contained"
                   fullWidth
                 >
-                  Add to basket
+                  {item ? "Update quantity" : "Add to basket"}
                 </Button>
               </Grid2>
             </Grid2>
